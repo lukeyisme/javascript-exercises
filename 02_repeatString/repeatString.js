758da4529da2031ea3126985a876080fc4c5bc99
Luke Curry <lukeyisme@gmail.com>
2022-11-08 19:50:28 -0500

    Finished the second round of test exercises, stringRepeat function passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    phrase = new String(string)
 
+    if(num == 0){
+        phrase = '';
+    }
+
+    else if(num == 1){
+        phrase = string;
+    }
+
+    else if(num >= 2){
+        for(i = 1; i < num; i++){
+            phrase += string;
+        }
+    }   
+    
+    else if(num < 0){
+        phrase = 'ERROR';
+    }
+
+    return phrase
 };
 
 // Do not edit below this line
