663ec93015c941f5f44d1462ae0ebe8a29340b94
Luke Curry <lukeyisme@gmail.com>
2022-11-09 20:26:26 -0500

    Wrote a function in reverseString.js to reverse a string, passed all four tests in reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+   let newStr = '';
+
+   for(let i = str.length - 1; i >= 0; i--){
+    newStr += str[i];
+   }
+
+   return newStr
 
 };
 
