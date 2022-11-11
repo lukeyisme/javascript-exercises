7edbc3d48b9856a616936e3efb27278c705dd335
Luke Curry <lukeyisme@gmail.com>
2022-11-10 20:53:46 -0500

    Passed the first two tests, needs to be reworked to ignore non-present values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    
+    argsArray = args;
+    numOfArgs = argsArray.length - 1;
 
+    for(let i = numOfArgs; i >= 0; i--){
+
+        let thingToDelete = argsArray[i];
+        let indexToDelete = inputArray.indexOf(thingToDelete)
+        inputArray.splice(indexToDelete, 1)
+
+    }
+
+    return inputArray
 };
 
 // Do not edit below this line
